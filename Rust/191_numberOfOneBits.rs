e46db8b08df2f1cde43bcba2030f843cbc3833f9
fr3drick545 <makh.paur545@gmail.com>
2023-06-15 18:15:45 -0400

    191_numberOfOneBits Rust Solution

---
+++ Rust/191_numberOfOneBits.rs
@@ -0,0 +1,58 @@
+/**
+ * Write a function that takes the binary representation of an unsigned
+ * integer and returns the number of '1' bits it has (also known as the Hamming weight).
+ * 
+ * Note:
+ * Note that in some languages, such as Java, there is no unsigned 
+ * integer type. In this case, the input will be given as a signed 
+ * integer type. It should not affect your implementation, as the integer's 
+ * internal binary representation is the same, whether it is signed or unsigned.
+ * 
+ * In Java, the compiler represents the signed integers using 2's complement 
+ * notation. Therefore, in Example 3, the input represents the signed integer. -3.
+ *  
+ * 
+ * Example 1:
+ * Input: n = 00000000000000000000000000001011
+ * Output: 3
+ * Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+ * 
+ * Example 2:
+ * Input: n = 00000000000000000000000010000000
+ * Output: 1
+ * Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+ * 
+ * Example 3:
+ * Input: n = 11111111111111111111111111111101
+ * Output: 31
+ * Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+ */
+impl Solution {
+    pub fn hammingWeight (n: u32) -> i32 {
+        let mut betterN = n;
+        let mut totalNumOfOnes: i32 = 0;
+
+        while (betterN != 0)
+        {
+            if (betterN & 1 == 1)
+            {
+                totalNumOfOnes += 1;
+            }
+
+            betterN >>= 1;
+        }
+
+        return totalNumOfOnes;
+
+        n.count_ones() as i32
+    }
+}
+
+/**
+ * One-Liner Solution
+ */
+impl Solution {
+    pub fn hammingWeight (n: u32) -> i32 {
+        return n.count_ones() as i32;
+    }
+}
